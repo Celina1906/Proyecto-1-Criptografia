284fb0a5257f06dbec409a2db88505d33047680d
Tavo0905 <gperezb2002@gmail.com>
2024-04-13 20:39:32 -0600

    Inversos Multiplicativos Modulos de N
    Se creó un código para calcular el inverso multiplicativo módulo N de un arreglo de números menores a N, siendo N un número primo grande.

---
+++ Parte2.py
@@ -0,0 +1,47 @@
+# Se crea una lista de enteros <= al numero primo
+# Sacar el inverso multiplicativo modulo N de cada numero
+# donde N es el número primo
+
+
+# Se importa la función generadora de números primos
+# y "sympy" para calcular el inverso multiplicativo
+import Parte1, sympy
+
+def invMultMod(nums, prime):
+    """Calcula el inverso multiplicativo módulo
+       N de cada número dentro de la lista con
+       respecto al número primo.
+
+    Args:
+        nums (list <int>): Lista de números enteros <= N
+        prime (int): Número primo grande
+
+    Returns:
+        dict: Diccionario con cada número de la lista y
+              su respectivo inverso multiplicativo
+              módulo de N.
+    """
+    
+    inv = {}
+    for num in nums:
+        try:
+            # Calcula el inverso multiplicativo módulo del número primo
+            inverse = sympy.mod_inverse(num, prime)
+            inv[num] = inverse
+        except ValueError:
+            # Controla el flujo en caso de no ser inverso multiplicativo
+            inv[num] = None
+    return inv
+
+# Se genera un número primo de 32 bits
+prime = Parte1.generate_prime(32)
+
+# Se crea una lista con enteros menores al número primo
+listNums = [x for x in range(1, 100001)]
+
+invsMultDict = invMultMod(listNums, prime)
+
+print(invsMultDict)
+
+
+
