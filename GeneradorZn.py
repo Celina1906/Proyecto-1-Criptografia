cc0bdd8f7fa361c9d7fc7a0321a9c40510f1328d
María José Porras <79280750+marijopm27@users.noreply.github.com>
2024-04-14 22:28:06 -0600

    Mejoras
    - Cambios y limpiezas de codigo

--- GeneradorZn.py
+++ GeneradorZn.py
@@ -1,6 +1,9 @@
 from math import gcd
 
-def es_primo(num):
+def is_prime(num):
+    """
+    Comprueba si un número es primo.
+    """
     if num < 2:
         return False
     for i in range(2, int(num**0.5) + 1):
@@ -8,51 +11,21 @@ def es_primo(num):
             return False
     return True
 
-def primer_primo_divisor(n):
+def first_prime_divisor(n):
+    """
+    Encuentra el primer divisor primo de un número.
+    """
     if n < 2:
         return None
     for i in range(2, int(n**0.5) + 1):
-        if n % i == 0 and es_primo(i):
+        if n % i == 0 and is_prime(i):
             return i
     return n
 
-
-def mcd(a, b):
+def euler_function(n):
     """
-    Calcula el máximo común divisor (MCD) de dos números utilizando el algoritmo de Euclides.
+    Calcula la función totiente de Euler para un número dado.
     """
-    while b != 0:
-        r=  a % b
-        a= b
-        b=r
-    return a
-
-
-def ord_shanks(a, n):
-    if gcd(a, n) != 1:
-        raise ValueError("a debe ser coprimo con n")
-    
-    m = int(n ** 0.5) + 1
-    alpha = pow(a, m, n)
-    beta = alpha
-    for i in range(1, m + 1):
-        beta = (beta * alpha) % n
-        if beta == 1:
-            return i * m
-    
-    return None
-
-def euler(n):
-    if n<1:
-        print("Error n debe ser mayor a uno")
-    else:
-        cont=0
-        for  i in range(1,n+1):
-            if mcd(i,n)==1:
-                cont=cont+1
-    return cont
-
-def euler2(n):
     if n < 1:
         print("Error: n debe ser mayor a uno")
         return None
@@ -71,30 +44,17 @@ def euler2(n):
         phi -= phi // n
 
     return phi
-         
-def orden(a, n):
-    for t in range(1, n):
-        if pow(a, t, n) == 1:#La función pow() toma dos argumentos obligatorios: la base y el exponente, y opcionalmente un tercer argumento que especifica el módulo (si se desea calcular la potencia modular).
-            return t
-    return None
 
-def genZn(n):
-    ordenZn=euler2(n)
+def generator_of_Zn(n):
+    """
+    Encuentra un generador del grupo multiplicativo Zn.
+    """
+    phi = euler_function(n)
+    p = first_prime_divisor(phi)
+    exp = round(phi/p)
     for alpha in range(1, n):
-        if(orden(alpha,n)==ordenZn):
-            print(alpha," Es un generador del grupo multiplicativo de Z",n)
-            break;
+        if pow(alpha, exp, n) != 1:
+            print(alpha, " es un generador del grupo multiplicativo de Z", n)
+            
 
-def genZn2(n):
-    phi=euler2(n) #Se puede cambiar por n-1 si es n es primo
-    p=primer_primo_divisor(phi)
-    print(p)
-   
-    exp=round(phi/p)
-    print(exp)
-    for alpha in range(1, n):
-        if pow(alpha,exp, n) != 1:#La función pow() toma dos argumentos obligatorios: la base y el exponente, y opcionalmente un tercer argumento que especifica el módulo (si se desea calcular la potencia modular).
-            print(alpha," Es un generador del grupo multiplicativo de Z",n)
-            
-            
-    
+generator_of_Zn(71)  
